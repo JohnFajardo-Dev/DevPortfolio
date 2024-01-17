631ed48a1ea377899f30a9949c212268f7775646
Fajardo-dev <johnfajardo177@gmail.com>
2024-01-17 15:24:25 -0500

    fixed cv

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 function Button({ title }) {
   return (
     <button className="button">
-      <a href="src/assets/files/John-Fajardo-CV.pdf">
+      <a href="assets/files/John-Fajardo-CV.pdf">
         <p>{title}</p>
       </a>
     </button>
