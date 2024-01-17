563133764963eac8067ebc6ab7f6b98dc050481f
Fajardo-dev <johnfajardo177@gmail.com>
2024-01-17 15:20:46 -0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -49,20 +49,20 @@ function App() {
           <Cards
             title="WeatherApp"
             description="WeatherApp is a simple application to check the weather for different cities around the world. "
-            image="src/assets/picture1.jpg"
+            image="public/assets/picture1.jpg"
             link="https://github.com/Fajardo-dev/WeatherApp"
             downstyle="true"
           />{" "}
           <Cards
             title='The "Fast Fourier Transform" (FFT)'
             description="This code analyzes the frequency content of a simple sine wave using the Fast Fourier Transform (FFT)."
-            image="src/assets/picture2.jpg"
+            image="public/assets/picture2.jpg"
             link="https://github.com/Fajardo-dev/FFT"
           />{" "}
           <Cards
             title="Portfolio v1 John Fajardo"
             description="Version 1 of John Fajardo's portfolio, made with html, css and javascript"
-            image="src/assets/picture3.jpg"
+            image="public/assets/picture3.jpg"
             downstyle="true"
             link="https://github.com/Fajardo-dev/Portfolio"
           />{" "}
