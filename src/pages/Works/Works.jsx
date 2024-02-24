d8bfb1a0e3bcf3f1cfd79260dbe93e106c771521
Fajardo-dev <johnfajardo177@gmail.com>
2024-02-24 15:38:54 -0500

    update works

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -8,20 +8,21 @@ function Works() {
         <Cards
           title="WeatherApp"
           description="WeatherApp is a simple application to check the weather for different cities around the world. "
-          image="assets/picture1.jpg"
+          image="assets/works/WeatherApp.jpg"
           link="https://github.com/Fajardo-dev/WeatherApp"
           downstyle="true"
         />{" "}
         <Cards
           title='The "Fast Fourier Transform" (FFT)'
           description="This code analyzes the frequency content of a simple sine wave using the Fast Fourier Transform (FFT)."
-          image="assets/picture2.jpg"
+          image="assets/works/FFT.jpg"
           link="https://github.com/Fajardo-dev/FFT"
+          downstyle="false"
         />{" "}
         <Cards
           title="VideoGame"
           description="In development"
-          image="assets/game.gif"
+          image="assets/works/VideoGame.gif"
           downstyle="false"
           link="#"
           indev="true"
@@ -29,9 +30,16 @@ function Works() {
         <Cards
           title="Portfolio v1 John Fajardo"
           description="Version 1 of John Fajardo's portfolio, made with html, css and javascript"
-          image="assets/picture3.jpg"
+          image="assets/works/PortfolioV1.jpg"
           downstyle="true"
           link="https://github.com/Fajardo-dev/Portfolio"
+        />{" "}
+        <Cards
+          title="GraffArt"
+          description="Graffiti art gallery, with an urban style"
+          image="assets/works/GraffArt.png"
+          downstyle="true"
+          link="https://github.com/Fajardo-dev/GraffArt"
         />{" "}
       </section>
     </section>
