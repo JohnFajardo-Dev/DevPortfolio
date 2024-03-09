0689ed5d9772a1cca3fab5f816e421aae109b146
Fajardo-dev <johnfajardo177@gmail.com>
2024-03-09 10:54:14 -0500

    Add Location component, remove margin button home

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,12 @@
+import "./Location.css";
+
+function Location() {
+  return (
+    <div className="ubi-cont">
+      <div className="ubi-state"></div>
+      <h2 className="ubi-txt">Bogota, Colombia</h2>
+    </div>
+  );
+}
+
+export default Location;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,17 +2,20 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "../../components/Button/Button";
 import "./Home.css";
+import Location from "../../components/Location/Location";
 
 function Home() {
   return (
     <section className="start-home" id="home">
       <article>
         <h1 className="focustxt">Hi, Im John!</h1>
+
         <h2 className="txthome">
           <span className="focustxt">Programmer</span> and{" "}
           <span className="focustxt">Web Developer</span> passionate about{" "}
           <span className="focustxt">design</span>
           <br /> and <span className="focustxt">opensource</span>
+          <Location />
         </h2>
         <Button title="Resume" />
       </article>
