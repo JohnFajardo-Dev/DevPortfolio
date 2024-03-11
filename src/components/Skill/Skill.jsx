8b69cbb62c053e1558bd8bf26e9d430d35d05fdb
Fajardo-dev <johnfajardo177@gmail.com>
2024-03-11 01:27:31 -0500

    edit skills style

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,66 +1,11 @@
 import "./Skill.css";
 import "../../App.css";
-import {
-  faHtml5,
-  faCss3Alt,
-  faJs,
-  faReact,
-  faPython,
-  faAndroid,
-  faNpm,
-  faNode,
-  faLinux,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// Componente individual para representar una habilidad
 function Skill({ name }) {
-  // Mapeo de nombres de habilidades a iconos correspondientes
-  const iconMapping = {
-    HTML: faHtml5,
-    CSS: faCss3Alt,
-    JavaScript: faJs,
-    React: faReact,
-    Python: faPython,
-    Android: faAndroid,
-    Npm: faNpm,
-    Node: faNode,
-    Linux: faLinux,
-  };
-
-  // Obtener el ícono correspondiente al nombre de habilidad
-  const icon = iconMapping[name];
-
   return (
     <div className="skill">
-      <FontAwesomeIcon icon={icon} size="5x" />
       <p>{name}</p>
     </div>
   );
 }
 
-// Componente contenedor que renderiza la lista de habilidades
-function SkillsContainer() {
-  const skills = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Python",
-    "Android",
-    "Npm",
-    "Node",
-    "Linux",
-  ];
-
-  return (
-    <div className="skills">
-      {/* Mapear cada habilidad a un componente Skill */}
-      {skills.map((skill, index) => (
-        <Skill key={index} name={skill} />
-      ))}
-    </div>
-  );
-}
-
-export default SkillsContainer;
+export default Skill;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,5 @@
 import "./About.css";
-import SkillsContainer from "../../components/Skill/Skill";
+import Skill from "../../components/Skill/Skill";
 function About() {
   return (
     <>
@@ -19,7 +19,33 @@ function About() {
           <img src="assets/profile.jpg" alt="" />
         </article>
       </section>
-      <SkillsContainer />
+      <section className="skills">
+        <h1></h1>
+        {/* Programming Languages */}
+        <Skill name="HTML 5" />
+        <Skill name="CSS 3" />
+        <Skill name="JAVASCRIPT" />
+        <Skill name="PYTHON" />
+        {/* Frameworks and Libraries */}
+        <Skill name="REACT" />
+        <Skill name="NPM" />
+        <Skill name="NODE.JS" />
+        {/* Databases */}
+        <Skill name="MONGODB" />
+        <Skill name="MYSQL" />
+        <Skill name="POSTGRES" />
+        {/* Software */}
+        <Skill name="VSCODE" />
+        <Skill name="VIM" />
+        <Skill name="GIT" />
+        <Skill name="GITHUB" />
+        <Skill name="LINUX" />
+        {/* Design */}
+        <Skill name="ADOBE PHOTOSHOP" />
+        <Skill name="ADOBE ILLUSTRATOR" />
+        <Skill name="ASEPRITE" />
+        <Skill name="FIGMA" />
+      </section>
     </>
   );
 }
