5fdc617564be69be2e702183845af87afced59be
Fajardo-dev <johnfajardo177@gmail.com>
2024-06-11 00:10:41 -0500

    fix code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,25 +4,25 @@ import { IconContext } from "react-icons";
 
 import { FaGithubAlt, FaExternalLinkAlt } from "react-icons/fa";
 
-function Cards({ title, description, image, href, skills, website, weblink }) {
+function Cards(props) {
   const { t } = useTranslation();
   return (
     <article>
       <div className="card">
-        <img src={image} alt="" className="img-card" />
+        <img src={props.image} alt="" className="img-card" />
 
         <article className="used-skills-cont">
-          <h3 className="used-skills-txt">{skills}</h3>
+          <h3 className="used-skills-txt">{props.skills}</h3>
         </article>
 
-        <h2 className="title-card">{title}</h2>
-        <p className="description-card">{description}</p>
+        <h2 className="title-card">{props.title}</h2>
+        <p className="description-card">{props.description}</p>
         <IconContext.Provider value={{ size: "1.25em" }}>
           <article className="btn-cont">
             <button
               className="button-card"
               onClick={() => {
-                window.open(href, "_blank");
+                window.open(props.href, "_blank");
               }}
             >
               <p>
@@ -30,11 +30,11 @@ function Cards({ title, description, image, href, skills, website, weblink }) {
                 <FaGithubAlt />
               </p>
             </button>
-            {website ? (
+            {props.website ? (
               <button
                 className="button-card"
                 onClick={() => {
-                  window.open(weblink, "_blank");
+                  window.open(props.weblink, "_blank");
                 }}
               >
                 <p>
