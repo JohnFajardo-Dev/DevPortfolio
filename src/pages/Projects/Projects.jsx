ec7eb274801711d5f6c9b2bd74dc7d3e116c22ed
John Fajardo <62899394+Fajardo-dev@users.noreply.github.com>
2024-07-20 14:31:40 -0500

    update

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -9,16 +9,6 @@ function Projects() {
       <h2 className="title-projects">{t("projects_title")}</h2>
       <section className="projects-cont">
         <Cards
-          title={t("weatherapp_title")}
-          description={t("weatherapp_description")}
-          image="assets/works/WeatherApp.jpg"
-          href="https://github.com/Fajardo-dev/WeatherApp"
-          skills="HTML CSS JS"
-          website="true"
-          weblink="https://fajardo-dev-weatherapp.netlify.app/"
-          category="Frontend"
-        />
-        <Cards
           title={t("graffart_title")}
           description={t("graffart_description")}
           image="assets/works/GraffArt.png"
@@ -27,24 +17,6 @@ function Projects() {
           website="true"
           weblink="https://graffart.netlify.app/"
           category="Frontend"
-        />
-        <Cards
-          title={t("truthescape_title")}
-          description={t("truthescape_description")}
-          image="assets/works/TruthEscape.gif"
-          href="http://github.com/Fajardo-dev/TruthEscape-WebSite"
-          skills="REACT"
-          website="true"
-          weblink="https://truthescape.netlify.app/"
-          category="Frontend"
-        />
-        <Cards
-          title={t("fft_title")}
-          description={t("fft_description")}
-          image="assets/works/FFT.jpg"
-          href="https://github.com/Fajardo-dev/FFT"
-          skills="PYTHON"
-          category="Software"
         />
       </section>
     </section>
