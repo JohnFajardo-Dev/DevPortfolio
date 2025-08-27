e5712a07c08780c5f76e8339a23b2e4ddbba0b35
Fajardo-dev <johnfajardo177@gmail.com>
2025-08-27 12:32:15 -0500

    Update projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -9,17 +9,6 @@ function Projects() {
       <h2 className="title-projects">{t("projects_title")}</h2>
       <section className="projects-cont">
         <Cards
-          title={t("taskapp_title")}
-          description={t("taskapp_description")}
-          image="assets/works/TaskApp.png"
-          href="https://github.com/Fajardo-dev/TaskApp"
-          skills="REACT"
-          repolink="true"
-          website="true"
-          weblink="https://apptotask.netlify.app/"
-          category="Frontend"
-        />
-        <Cards
           title={t("fshop_title")}
           description={t("fshop_description")}
           image="assets/works/FShop.png"
@@ -27,17 +16,6 @@ function Projects() {
           website="true"
           weblink="https://fshopfront.netlify.app/"
           category="FullStack"
-        />
-        <Cards
-          title={t("tattooportfolio_title")}
-          description={t("tattooportfolio_description")}
-          image="assets/works/TattooPortfolio.png"
-          skills="REACT"
-          website="true"
-          weblink="https://tattooportfolio.netlify.app/"
-          repolink="true"
-          href="https://github.com/JohnFajardo-Dev/TattooPortfolio"
-          category="Frontend"
         />
       </section>
     </section>
